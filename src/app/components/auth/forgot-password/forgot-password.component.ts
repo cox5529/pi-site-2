81981a1589acb8b12ab6fa2dc6fa3695cdce0054
Brandon Cox <cox5529@gmail.com>
2020-12-23 15:45:50 -0600

    Clean up some user pages

--- src/app/components/auth/forgot-password/forgot-password.component.ts
+++ src/app/components/auth/forgot-password/forgot-password.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { ForgotPasswordRequest } from 'src/app/models/requests/forgot-password-request';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-forgot-password",
-  templateUrl: "./forgot-password.component.html",
-  styleUrls: ["./forgot-password.component.sass"],
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.sass'],
 })
 export class ForgotPasswordComponent implements OnInit {
   form: FormGroup;
@@ -15,7 +16,8 @@ export class ForgotPasswordComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private snackBar: MatSnackBar,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -31,10 +33,11 @@ export class ForgotPasswordComponent implements OnInit {
 
     const request = new ForgotPasswordRequest();
     request.email = this.form.get('email').value;
-    
-    var response = await this.authService.forgotPassword(request);
+
+    const response = await this.authService.forgotPassword(request);
     if (response.ok) {
       this.snackBar.open('You will receive an email with a link to reset your password soon.', 'Dismiss');
+      await this.router.navigateByUrl('/auth/login');
     } else {
       this.snackBar.open('Something went wrong. Please try again later.', 'Dismiss');
     }
